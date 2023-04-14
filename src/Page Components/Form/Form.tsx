ed72060080701ec346d0009ee4651444473a7ff2
Sameep Rana <sameep.monsoon@gmail.com>
2023-04-15 02:31:25 +0545

    axios- formik-yup added- validation and api call -- half way there

--- src/Page Components/Form/Form.tsx
+++ src/Page Components/Form/Form.tsx
@@ -3,29 +3,82 @@ import "./Form.scss";
 import { useNavigate } from "react-router-dom";
 import { TextField } from "../../Components/TextField/TextField";
 import { AiFillAmazonSquare } from "react-icons/ai";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { HTTPMethods } from "../../Utils/HTTPMethods";
+
 const Form = (props: { children: React.ReactNode; type: string }) => {
   const { children, type } = props;
   const navigate = useNavigate();
+  // yup validation
+  let Schema = yup.object().shape({
+    username: yup.string().required("username is required."),
+    email: yup.string().required("Email is required."),
+    password: yup
+      .string()
+      .min(5, "At least 5 characters required")
+      .required("Password is required"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password")], "Passwords must match"),
+  });
+  // formik form handling
+  const formik = useFormik({
+    initialValues: {},
+    onSubmit: (values, action) => {
+      HTTPMethods.post("/auth/signup", values)
+        .then((res: any) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    },
+    validationSchema: Schema,
+  });
+
   if (type.toLowerCase() === "signup")
     return (
       <div className="form-main-div">
         <span className="form-amazon-icon">
           <AiFillAmazonSquare size={50} />
         </span>
-        <div className="form-container">
+        <form className="form-container" onSubmit={formik.handleSubmit}>
           <h2>{type}</h2>
           <div className="textfields">
-            <TextField type="email" name="email" placeholder="Email" />
-            <TextField type="password" name="password" maxLength={10} />
-            <TextField type="password" name="confirm password" maxLength={10} />
+            <TextField
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={formik.handleChange}
+            />
+
+            <TextField
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={formik.handleChange}
+            />
+            <TextField
+              type="password"
+              name="password"
+              maxLength={10}
+              onChange={formik.handleChange}
+            />
+            <TextField
+              type="password"
+              name="confirmPassword"
+              maxLength={10}
+              onChange={formik.handleChange}
+            />
           </div>
-          <button>sign up</button>
+          <button type="submit">sign up</button>
           <p>
             By signing-up you agree to Amazon's Condition of Use & Sale. Please
             see out Privacy Notice , our Cookies Notice and our interest-Based
             Ads Notice.
           </p>
-        </div>
+        </form>
       </div>
     );
   else
