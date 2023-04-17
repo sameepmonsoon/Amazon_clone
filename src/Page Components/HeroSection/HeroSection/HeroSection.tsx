8ab44f9f0d98f188479762cf550552dfdaa65cc5
Sameep Rana <sameep.monsoon@gmail.com>
2023-04-17 19:40:33 +0545

    filter completed

--- src/Page Components/HeroSection/HeroSection/HeroSection.tsx
+++ src/Page Components/HeroSection/HeroSection/HeroSection.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import "./HeroSection.scss";
 import Card from "../../../Components/Cards/ProductCard/Card";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../../../store/cartSlice";
 import { fetchProducts } from "../../../store/productSlice";
 import Cookies from "js-cookie";
+import { UserOrder } from "../../../Layout/HomeLayout";
 
 const HeroSection = () => {
   const dispatch = useDispatch();
@@ -12,7 +13,7 @@ const HeroSection = () => {
   function addToBasket(value: any) {
     dispatch(addToCart(value));
   }
-
+  const { search } = useContext(UserOrder);
   const [cartItems, setCartItems] = useState<any>(
     JSON.parse(Cookies.get("cartItems") || "[]")
   );
@@ -25,62 +26,72 @@ const HeroSection = () => {
     dispatch(fetchProducts());
   }, []);
   return (
-    <div className="hero">
-      <div className="hero-image-container">
-        <img
-          className="hero-image"
-          src="https://images.unsplash.com/photo-1633174524778-61a18ee54490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1196&q=80"
-          alt=""
-        />
-      </div>
-      <div className="products-dual">
-        {products.slice(10, 12).map((item: any, index: number) => {
-          return (
-            <Card
-              id={item.id}
-              image={item.image}
-              category={item.category}
-              description={item.description}
-              button={
-                <button
-                  onClick={() => {
-                    addToBasket({ ...item, quantity: 1 });
-                  }}>
-                  Add to cart
-                </button>
-              }
-              ratings={item.rating.rate}
-              price={item.price}
-              key={index}
+    <UserOrder.Consumer>
+      {(value: any) => (
+        <div className="hero">
+          <div className="hero-image-container">
+            <img
+              className="hero-image"
+              src="https://images.unsplash.com/photo-1633174524778-61a18ee54490?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1196&q=80"
+              alt=""
             />
-          );
-        })}
-      </div>
-      <div className="products">
-        {products.map((item: any, index: number) => {
-          return (
-            <div key={index}>
-              <Card
-                id={item.id}
-                image={item.image}
-                category={item.category}
-                description={item.description}
-                button={
-                  <button
-                    onClick={() => {
-                      addToBasket({ ...item, quantity: 1 });
-                    }}>
-                    Add to cart
-                  </button>
-                }
-                ratings={item.rating.rate}
-                price={item.price}
-              />
-            </div>
-          );
-        })}
-      </div>
-    </div>
+          </div>
+          <div className="products-dual">
+            {products.filter((item: any) =>
+                item.category.toLowerCase().startsWith(search)
+              ).slice(10, 12).map((item: any, index: number) => {
+              return (
+                <Card
+                  id={item.id}
+                  image={item.image}
+                  category={item.category}
+                  description={item.description}
+                  button={
+                    <button
+                      onClick={() => {
+                        addToBasket({ ...item, quantity: 1 });
+                      }}>
+                      Add to cart
+                    </button>
+                  }
+                  ratings={item.rating.rate}
+                  price={item.price}
+                  key={index}
+                />
+              );
+            })}
+          </div>
+          <div className="products">
+            {products
+              .filter((item: any) =>
+                item.category.toLowerCase().startsWith(search)
+              )
+              .map((item: any, index: number) => {
+                return (
+                  <div key={index}>
+                    <Card
+                      id={item.id}
+                      image={item.image}
+                      category={item.category}
+                      description={item.description}
+                      button={
+                        <button
+                          onClick={() => {
+                            addToBasket({ ...item, quantity: 1 });
+                          }}>
+                          Add to cart
+                        </button>
+                      }
+                      ratings={item.rating.rate}
+                      price={item.price}
+                    />
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      )}
+    </UserOrder.Consumer>
   );
 };
 
